3a3964b51e247930a6d228217d548d2e9432ea01
yongnan <ync@fastmail.com>
2021-06-01 16:21:47 +0800

    updating

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected } from "./utils/interact.js";
 
 const Minter = (props) => {
 
@@ -9,17 +10,48 @@ const Minter = (props) => {
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
  
-  useEffect(async () => { //TODO: implement
-    
+  useEffect(async () => {
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status); 
+
+    addWalletListener(); 
   }, []);
 
   const connectWalletPressed = async () => { //TODO: implement
-   
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
   };
 
   const onMintPressed = async () => { //TODO: implement
     
   };
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
   return (
     <div className="Minter">
